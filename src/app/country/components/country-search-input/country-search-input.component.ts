263cf51d3e961f2c9ac4c20a136097fccb7ff296
Jayro Salazar <megajjks@gmail.com>
2025-06-10 11:07:00 -0600

    fix(base): create base and schema of app

---
+++ src/app/country/components/country-search-input/country-search-input.component.ts
@@ -0,0 +1,11 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './country-search-input.component.html',
+})
+export class CountrySearchInputComponent {
+  placeholder = input('Buscar');
+  value = output<string>();
+}
--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -3,12 +3,8 @@ import { CountrySearchInputComponent } from '../../components/country-search-inp
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 
 @Component({
-  selector: 'app-by-capital-page',
+  selector: 'app-by-region-page',
   imports: [CountrySearchInputComponent, CountryListComponent],
-  templateUrl: './by-capital-page.component.html',
+  templateUrl: './by-region.component.html',
 })
-export class ByCapitalPageComponent {
-  onSearch(value: string) {
-    console.log({ value });
-  }
-}
+export class ByRegionPageComponent {}
